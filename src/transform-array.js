3c447955440481f6dc267448843aa230d23cd093
Ulugbek <ulugb3k270@gmail.com>
2024-10-29 15:55:46 +0500

    solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`\'arr\' parameter must be an instance of the Array!`);
+  }
+
+  const result = [];
+  let skipNext = false;
+  let wasLastElementDiscarded = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    const currentItem = arr[i];
+
+    switch (currentItem) {
+      case "--double-next":
+        if (i + 1 < arr.length) {
+          result.push(arr[i + 1], arr[i + 1]);
+          i++;
+        }
+        wasLastElementDiscarded = false;
+        break;
+
+      case "--double-prev":
+        if (result.length > 0 && !skipNext && !wasLastElementDiscarded) {
+          const prevElement = result[result.length - 1];
+          result.push(prevElement);
+        }
+        wasLastElementDiscarded = false;
+        skipNext = false;
+        break;
+
+      case "--discard-prev":
+        if (result.length > 0 && !wasLastElementDiscarded) {
+          result.pop();
+        }
+        wasLastElementDiscarded = false;
+        skipNext = false;
+        break;
+
+      case "--discard-next":
+        skipNext = true;
+        wasLastElementDiscarded = true;
+        i++;
+        break;
+
+      default:
+        if (!skipNext) {
+          result.push(currentItem);
+          wasLastElementDiscarded = false;
+        }
+        skipNext = false;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  transform
+  transform,
 };
